34424df72daf05a2273372da4094e9f17b4ffb19
a64 <a64@iMac-van-a64.local>
2026-04-02 19:59:05 +0200

    fix: cache + service worker + sync

--- assets/app-live.js
+++ assets/app-live.js
@@ -559,6 +559,7 @@ function openAdd() {
   editTitle.textContent = "Tornooi toevoegen";
 
   fDate.value = todayLocalISO();
+  fStatus.value = "";
   fTime.value = "";
   fClub.value = "";
   fSpel.value = "";
